fb24b145f69897347e6d183571e9492da105d48b
Your Name <you@example.com>
2020-09-09 20:17:10 +0530

    Updates

---
+++ precache-manifest.28f35180f241b2d75e80dc904ae6af57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356e8d01e016d0cd88205ab0e1d43dda",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3fb5248ab3caa2a15968",
+    "url": "/monsters-rolodex/static/css/main.1a9d6464.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb5248ab3caa2a15968",
+    "url": "/monsters-rolodex/static/js/main.325c7f60.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
